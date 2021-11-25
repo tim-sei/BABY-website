7766774d0216c9bc1eeb2ab20228bef0053271bc
Tim Seifert <tim.seifert@gmx.de>
2021-11-25 17:30:41 +0100

    Added the temperature slider to frontend

--- app.py
+++ app.py
@@ -33,12 +33,14 @@ st.markdown("""#  -- - BABY - --
 model_type = st.radio('Which model do you want to try?',
                     ('ada', 'curie', 'babbage', 'davinci'))
 
+temperature = st.slider('Select a temperature', 1, 10, 1)
+
 prompt = st.text_input('I want to know: Can beta.BABY give the right answer to my joke...', 'Type in my joke...')
 
 secrets = st.text_input('Password', 'write password here')
 
 url = 'https://morning-citadel-09821.herokuapp.com/predict'
-X = dict(model=model_type, prompt=prompt, secret=secrets)
+X = dict(model=model_type, prompt=prompt, secret=secrets, temperature=temperature/10)
 
 # 3. Let's call our API using the `requests` package...
 #response = requests.get(url, params=X)
