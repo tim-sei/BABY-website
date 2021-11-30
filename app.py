f6d35d66f6cf593726f175233ef722cf28866919
Guillaume DeSa <gdesa.317@gmail.com>
2021-11-30 14:15:48 +0100

    frontend changes

--- app.py
+++ app.py
@@ -35,13 +35,13 @@ st.markdown("""#  -- - BABY - --
 model_type = st.radio('Which model do you want to try?',
                     ('ada', 'curie', 'babbage', 'davinci'))
 
-hatespeech = st.radio('Activate Hate Speech Detecor', (True, False))
+# hatespeech = st.radio('Activate Hate Speech Detecor', (True, False))
 
-temperature = st.slider('Select a temperature', 1, 10, 1)
+# temperature = st.slider('Select a temperature', 1, 10, 1)
 
 n = st.slider('Select number of generations', 1, 3, 1)
 
-max_tokens = st.slider('Select max tokens', 25, 75, 1)
+# max_tokens = st.slider('Select max tokens', 25, 75, 1)
 
 secrets = st.text_input('Password', '')
 
@@ -59,6 +59,7 @@ with col1:
                         'rap song',
                         'classical poem',
                         'bob dylan song'))
+
 with col2:
     st.subheader("...written by...")
     personality_type = st.radio('', (
@@ -69,19 +70,13 @@ with col2:
         'an artificial intelligence'
     ))
 
-
-
 url = 'https://morning-citadel-09821.herokuapp.com/predict'
 
 X = dict(model=model_type,
          prompt=prompt,
          personality_type=personality_type,
          secret=secrets,
-         temperature=temperature / 10,
-         n=n,
-         max_tokens=max_tokens,
-         _type=answer_type,
-         HateSpeechDetector=hatespeech)
+         _type=answer_type)
 
 # 3. Let's call our API using the `requests` package...
 #response = requests.get(url, params=X)
@@ -97,8 +92,8 @@ if st.button('Feed BABY.'):
     poem = call_api()
     # for poem in poems:
     # poem = poem.replace("\n===", "")
-    st.markdown(f'## This is a {answer_type}, written by the fake personality of {personality_type} about {prompt}')
-    st.markdown(f'{poem}')
+    st.markdown(f'## This is a {answer_type}, written by the fake personality of {personality_type} about {prompt}:')
+    st.markdown(f'## {poem}')
 
 '''### ____BABY____ is:
 
